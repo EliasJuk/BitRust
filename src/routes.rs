31b5180b8a8ffd743099f761d2159ec1b6a21521
eliasjuk <eliasp.juk@hotmail.com>
2024-11-11 17:17:36 -0300

    solicita a chave privada e faz a conversao

---
+++ src/routes.rs
@@ -0,0 +1,36 @@
+// src/routes.rs
+
+use crate::utils::utils;
+use crate::functions::wif_generate;
+
+use std::io;
+use std::io::Write;
+use colored::*;
+
+pub fn ask_for_private_key_and_generate_wif() {
+	// Limpar a tela e exibir o menu
+	utils::clear_console();
+	println!("Example: b3d8c534b0d494ccda1b4dcc47cdc0aa701e39f89c326035f73558e590163dcb");
+
+	// Pergunta para o usuário digitar a chave privada em formato hexadecimal
+	print!("\nDigite a chave privada em formato hexadecimal: ");
+	io::stdout().flush().unwrap();
+
+	let mut private_key_hex = String::new();
+	io::stdin()
+		.read_line(&mut private_key_hex)
+		.expect("Falha ao ler a chave privada");
+	let private_key_hex = private_key_hex.trim(); // Remove espaços ou quebras de linha extras
+
+	// Chama a função para gerar a chave WIF
+	match wif_generate::key_to_wif(private_key_hex) {
+		Ok(wif) => {
+			println!("\nChave WIF gerada: {}", wif.yellow().bold());
+		}
+		Err(e) => {
+			println!("\nErro: {}", e);
+		}
+	}
+
+	utils::pause_until_enter();
+}
