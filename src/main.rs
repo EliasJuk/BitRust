f0a41eaa6b5b5a048fbd33edcddd06379bfbc1e5
eliasjuk <eliasp.juk@hotmail.com>
2024-11-11 16:07:16 -0300

    Criado menu principal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+// src/main.rs
+
+use std::io;
+
+mod utils {
+	pub mod utils;
+}
+
 fn main() {
-    println!("Hello, world!");
+	loop {
+		// Limpar a tela e exibir o menu
+		utils::utils::clear_console();
+
+		println!("╔═══════════════════════════════╗");
+		println!("║                               ║");
+		println!("║           BIT RUST            ║");
+		println!("║             v0.1              ║");
+		println!("║                               ║");
+		println!("╚═══════════════════════════════╝");
+		println!();
+		println!("-------------------------------------------");
+		println!(" 1 - Opção 1 ");
+		println!(" 2 - Opção 2 ");
+		println!(" 3 - Opção 3 ");
+		println!("-------------------------------------------");
+		println!(" 4 - Opção 4 ");
+		println!(" 5 - Opção 5 ");
+		println!(" 6 - Opção 6 ");
+		println!("-------------------------------------------");
+		println!(" 7 - Opção 7 ");
+		println!(" 8 - Opção 8 ");
+		println!(" 9 - Opção 9 ");
+		println!("-------------------------------------------");
+		println!(" 0 - EXIT");
+		println!("-------------------------------------------");
+
+		// Solicitar a escolha do usuário
+		print!("Escolha uma opção: ");
+		
+
+		let mut choice = String::new();
+		io::stdin()
+			.read_line(&mut choice)
+			.expect("Falha ao ler a entrada");
+		let choice: i32 = match choice.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Opção inválida. Tente novamente.");
+				continue;
+			}
+		};
+
+		// Processar a escolha do usuário
+		match choice {
+			1 => println!("Opção 1"),
+			2 => println!("Opção 2"),
+			3 => println!("Opção 3"),
+			4 => println!("Opção 4"),
+			5 => println!("Opção 5"),
+			0 => {
+				println!("Saindo...");
+				break;
+			}
+			_ => println!("Opção inválida. Tente novamente."),
+		}
+	}
 }
