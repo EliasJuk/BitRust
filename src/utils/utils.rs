9ab5e125f8e9fc49a4290b7fafc6a2c733d55ea5
eliasjuk <eliasp.juk@hotmail.com>
2024-11-11 17:13:11 -0300

    criado funcao pause

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -7,3 +7,12 @@ pub fn clear_console() {
 	print!("\x1b[2J\x1b[1;1H");
 	io::stdout().flush().unwrap();
 }
+
+/// Função para pausar a execução até o usuário pressionar Enter
+pub fn pause_until_enter() {
+	print!("\nPressione Enter para voltar ao menu... ");
+	io::stdout().flush().expect("Falha ao exibir a mensagem");
+
+	let mut _dummy = String::new();
+	io::stdin().read_line(&mut _dummy).expect("Falha ao esperar entrada");
+}
