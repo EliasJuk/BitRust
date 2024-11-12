ccf778c471e9399ac1c5660ada14ef9db16c4ec0
eliasjuk <eliasp.juk@hotmail.com>
2024-11-12 16:11:32 -0300

    calcular combinações

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,6 +1,9 @@
 // src/utils/utils.rs
 
 use std::io::{self, Write};
+use num_bigint::BigUint;
+use num_traits::One;
+
 
 /// Função para limpar a tela do terminal
 pub fn clear_console() {
@@ -15,4 +18,47 @@ pub fn pause_until_enter() {
 
 	let mut _dummy = String::new();
 	io::stdin().read_line(&mut _dummy).expect("Falha ao esperar entrada");
+}
+
+/// Função para calcular o número de combinações entre o StartHash e o EndHash
+pub fn calcular_combinacoes(start_hash: &str, end_hash: &str) -> Result<BigUint, String> {
+	// Convertendo as strings hexadecimais para BigInt
+	let start = match hex_to_bigint(start_hash) {
+		Ok(value) => value,
+		Err(e) => return Err(format!("Erro no StartHash: {}", e)),
+	};
+
+	let end = match hex_to_bigint(end_hash) {
+		Ok(value) => value,
+		Err(e) => return Err(format!("Erro no EndHash: {}", e)),
+	};
+
+	// Calcula a diferença entre os dois valores
+	let diff = if end > start {
+		end - start
+	} else {
+		start - end
+	};
+
+	Ok(diff + BigUint::one())
+}
+
+/// Função para converter de hexadecimal para BigInt
+pub fn hex_to_bigint(hex: &str) -> Result<BigUint, String> {
+	let hex = hex.trim().to_lowercase(); // Remove espaços e converte para minúsculas
+
+	// Verifica se o valor é "null" ou está vazio
+	if hex.is_empty() || hex == "null" {
+		return Err("Valor inválido: 'null' ou vazio.".to_string());
+	}
+
+	// Verifica se a string contém apenas caracteres hexadecimais
+	if hex.chars().all(|c| c.is_digit(16)) {
+		match BigUint::parse_bytes(hex.as_bytes(), 16) {
+			Some(value) => Ok(value),
+			None => Err("Erro na conversão do valor hexadecimal para BigInt.".to_string()),
+		}
+	} else {
+		Err("A string contém caracteres não hexadecimais.".to_string())
+	}
 }
