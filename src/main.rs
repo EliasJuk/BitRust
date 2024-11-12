1e9bfa612c629eb29da016bc1f50bcc78ce5d281
eliasjuk <eliasp.juk@hotmail.com>
2024-11-12 16:53:51 -0300

    add funcao ao menu

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod utils {
 mod modules {
 	pub mod wif_generate;
 	pub mod desafio_info;
+	pub mod private_to_public_key;
 }
 
 mod data {
@@ -29,20 +30,20 @@ fn main() {
     println!("{}", format!("║           {}            ║", "BIT RUST".blue().bold()).truecolor(190, 60, 190));
     println!("{}", format!("║             {}              ║", "v0.1".yellow().bold()).truecolor(190, 60, 190));
 		println!("{}","║                               ║".bold().truecolor(190, 60, 190));
-		println!("{}","╚═══════════════════════════════╝".bold().truecolor(190, 60, 190));
+		println!("{}","╚═by CaptainJuk═════════════════╝".bold().truecolor(190, 60, 190));
 		println!();
 		println!("-------------------------------------------");
 		println!(" 1 - CONVERT PRIVATE KEY TO WIF");
-		println!(" 2 - Opção 2 ");
-		println!(" 3 - Opção 3 ");
+		println!(" 2 - GENERATE PUBLIC KEY");
+		println!(" 3 -  ");
 		println!("-------------------------------------------");
-		println!(" 4 - Opção 4 ");
-		println!(" 5 - Opção 5 ");
-		println!(" 6 - Opção 6 ");
+		println!(" 4 -  ");
+		println!(" 5 -  ");
+		println!(" 6 -  ");
 		println!("-------------------------------------------");
 		println!(" 7 - DESAFIO INFO");
-		println!(" 8 - Opção 8 ");
-		println!(" 9 - Opção 9 ");
+		println!(" 8 -  ");
+		println!(" 9 -  ");
 		println!("-------------------------------------------");
 		println!(" 0 - EXIT");
 		println!("-------------------------------------------");
@@ -67,7 +68,7 @@ fn main() {
 		// Processar a escolha do usuário
 		match choice {
 			1 => menu_task::generate_wif(),
-			2 => println!("Opção 2"),
+			2 => menu_task::generate_public_key(),
 			3 => println!("Opção 3"),
 			4 => println!("Opção 4"),
 			5 => println!("Opção 5"),
